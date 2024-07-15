c86e95cadc320bb68b72510bbffab24f1e189397
AmitMaymon <145137599+AmitMaymon@users.noreply.github.com>
2024-07-15 23:24:00 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://schoolAppTester:A123456@cluster0.ajdwy4s.mongodb.net/studentDB?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://schoolAppTester:A123456@cluster0.ajdwy4s.mongodb.net/studentDB?retryWrites=true&w=majority') // Add your own cluster or use this restricted one
     .then(() => { console.log("Database is connected!"); })
     .catch((err) => { console.log(err); })
